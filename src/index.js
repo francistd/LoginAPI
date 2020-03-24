b01e0f6f2e94dff20376b6bb279e0e6384fc91cc
Juan <tava24@hotmail.com>
2020-03-24 12:46:36 -0400

    configuracion inicial

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+// inicializacion 
+const app = express();
+
+
+// Configuracion
+app.set('port', process.env.PORT || 4000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: path.join(app.get('views'), 'layouts'),
+	partialsDir: path.join(app.get('views'), 'partials'),
+	extname: '.hbs',
+	helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs'); 
+
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+// Variables Globales
+app.use((req,res, next)=> { 
+	next();
+});
+
+// Routes
+app.use(require('./routes/'));
+app.use(require('./routes/authentication'));
+app.use(require('./routes/links'));
+
+// Publica
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciamos el servidor
+app.listen(app.get('port'), ()=> {
+	console.log('Servidor en puerto', app.get('port'));
+});
